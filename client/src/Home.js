e194480ddbb5dddbfca421383c3ce752087ab342
Christy Perozzi <christy.perozzi@gmail.com>
2022-05-13 09:52:44 -0400

    changing urls to heroku

--- client/src/Home.js
+++ client/src/Home.js
@@ -6,14 +6,14 @@ function Home(){
             <h2>Home  </h2>
             <p> This web app is a REST API intended for other developers to use for text in their own projects.  </p>
             <h3> Available endpoints:</h3>
-            <h4>Quotes</h4>
-            <p> url/quotes (will edit this with url after deploying to Heroku) <a href="http://localhost:3000/quotes" > TRY IT!  </a></p>
+            <h4>Quotes:</h4>
+            <p> Send requests to https://wonderland-api.herokuapp.com/quotes <a href="https://wonderland-api.herokuapp.com/quotes" > TRY IT!  </a></p>
             
-            <h4>Characters</h4>
-            <p> url/characters (will edit this with url after deploying to Heroku) <a href="http://localhost:3000/characters" > TRY IT!  </a></p>
+            <h4>Characters:</h4>
+            <p> Send requests to https://wonderland-api.herokuapp.com/characters <a href="https://wonderland-api.herokuapp.com/characters" > TRY IT!  </a></p>
             
-            <h4>Books</h4>
-            <p> url/books (will edit this with url after deploying to Heroku) <a href="http://localhost:3000/books" > TRY IT!  </a></p>
+            <h4>Books:</h4>
+            <p> Send requests to https://wonderland-api.herokuapp.com/books <a href="https://wonderland-api.herokuapp.com/books" > TRY IT!  </a></p>
             
 
         </div>
