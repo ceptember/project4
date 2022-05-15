afa3ceca7cc67ec7cd606aaffd6e31b09a78c98b
Christy Perozzi <christy.perozzi@gmail.com>
2022-05-15 19:48:04 -0400

    adding text for footer component

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -4,7 +4,12 @@ function Footer (){
 
     return (
         <div className="footer">
-            <p>Footer component</p>
+        <span>© 2022 Christy Perozzi</span>
+        <br />
+        <span>Follow Me: &nbsp;  
+         <a className = "footer-link" href ="http://www.linkedin.com/in/chrizzi"><i className="fab fa-linkedin"></i> </a>
+         <a className = "footer-link" href="https://github.com/ceptember?tab=repositories"><i className="fab fa-github-square"></i></a>
+        </span>
         </div>
 
     )
